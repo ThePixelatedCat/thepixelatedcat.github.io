931ee9c331be92116ccdb89459e3b4a933fad2bf
ThePixelatedCat <acfrostie@gmail.com>
2025-06-13 13:15:00 +1000

    Added placeholders for latest outputs, updated js

--- scripts/main.js
+++ scripts/main.js
@@ -1,79 +1,8 @@
-// Store a reference to the <h1> in a variable
-const myHeading = document.querySelector("h1");
+// Store a reference to the last listened music field
+const lastListened = document.querySelector("samp[id='lastlistened'");
+// Store a reference to the last played steam game field
+const lastPlayed = document.querySelector("samp[id='lastplayed'");
 
-const generateRandomString = (length) => {
-  const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  const values = crypto.getRandomValues(new Uint8Array(length));
-  return values.reduce((acc, x) => acc + possible[x % possible.length], "");
-}
 
-const sha256 = async (plain) => {
-  const encoder = new TextEncoder()
-  const data = encoder.encode(plain)
-  return window.crypto.subtle.digest('SHA-256', data)
-}
+http://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/?key=XXXXXXXXXXXXXXXXX&steamid=76561197960434622&format=json
 
-const base64encode = (input) => {
-  return btoa(String.fromCharCode(...new Uint8Array(input)))
-    .replace(/=/g, '')
-    .replace(/\+/g, '-')
-    .replace(/\//g, '_');
-}
-
-const codeVerifier  = generateRandomString(64);
-const hashed = await sha256(codeVerifier)
-const codeChallenge = base64encode(hashed);
-
-const clientId = '779585e399734f60a93bb536804cb3c9';
-const redirectUri = 'thepixelatedcat.github.io';
-
-const scope = 'user-read-private user-read-email';
-const authUrl = new URL("https://accounts.spotify.com/authorize")
-
-// generated in the previous step
-window.localStorage.setItem('code_verifier', codeVerifier);
-
-const params =  {
-  response_type: 'code',
-  client_id: clientId,
-  scope,
-  code_challenge_method: 'S256',
-  code_challenge: codeChallenge,
-  redirect_uri: redirectUri,
-}
-
-authUrl.search = new URLSearchParams(params).toString();
-window.location.href = authUrl.toString();
-
-const urlParams = new URLSearchParams(window.location.search);
-let code = urlParams.get('code');
-
-const getToken = async code => {
-
-  // stored in the previous step
-  const codeVerifier = localStorage.getItem('code_verifier');
-
-  const url = "https://accounts.spotify.com/api/token";
-  const payload = {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
-    },
-    body: new URLSearchParams({
-      client_id: clientId,
-      grant_type: 'authorization_code',
-      code,
-      redirect_uri: redirectUri,
-      code_verifier: codeVerifier,
-    }),
-  }
-
-  const body = await fetch(url, payload);
-  const response = await body.json();
-
-  localStorage.setItem('access_token', response.access_token);
-}
-
-await getToken(code)
-
-myHeading.textContent = localStorage.getItem('access_token');
