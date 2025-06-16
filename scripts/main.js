9ab1c1d9a9fbfd5ddc1504ac3464ee3f0006a9ca
ThePixelatedCat <acfrostie@gmail.com>
2025-06-16 10:45:34 +1000

    Https steam api call, formatting

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,7 @@
 const lastListened = document.querySelector("samp[id='lastlistened'")
 // Store a reference to the last played steam game field
 const lastPlayed = document.querySelector("samp[id='lastplayed'")
-const steamFetchURL = 'http://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/?key=41DF567773130D0FBEE3ED9515CA24AD&steamid=76561198397546598&count=1'
+const steamFetchURL = 'https://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/?key=41DF567773130D0FBEE3ED9515CA24AD&steamid=76561198397546598&count=1'
 
 fetch(steamFetchURL)
     .then(response => response.json())
